41f51c8ef8b169e2c90efb2668cb66831a68cb18
Tempystral <elijah.doern@gmail.com>
2025-05-04 18:25:42 -0400

    And this panel was easy

--- src/dashboard/panels/AutoRecordSettingsPanel.vue
+++ src/dashboard/panels/AutoRecordSettingsPanel.vue
@@ -1,8 +1,28 @@
 <script setup lang="ts">
-const settings = nodecg.Replicant("settings");
-const autoRecord = nodecg.Replicant("autoRecord");
+import { useReplicant } from "nodecg-vue-composable";
+import Button from "primevue/button";
+import InputText from "primevue/inputtext";
+import { NAMESPACE } from "../utils";
+import { AutoRecordSettings, SettingsReplicant } from "@nmc/types";
+import { ref } from "vue";
+import DataTable from "primevue/datatable";
+import Column from "primevue/column";
+import FloatLabel from "primevue/floatlabel";
+import IconField from "primevue/iconfield";
+import SvgIcon from "@jamescoyle/vue-icon";
+import { mdiCheckCircleOutline } from "@mdi/js";
+import InputIcon from "primevue/inputicon";
+//import {autoRecord} from "defaultValues"
 
-window.onload = () => {
+const settings = useReplicant<SettingsReplicant>("settings", NAMESPACE);
+const autoRecord = useReplicant<AutoRecordSettings>("autoRecord", NAMESPACE, {
+  defaultValue: {
+    active: false,
+    filenameFormatting: "%CCYY-%MM-%DD %hh-%mm-%ss",
+  },
+});
+
+/* window.onload = () => {
   NodeCG.waitForReplicants(settings, autoRecord).then(() => {
     settings.on("change", (newVal) => {
       const autoRecord = document.getElementById("autoRecord");
@@ -28,8 +48,8 @@ window.onload = () => {
     );
   });
 };
-
-function toggleTable() {
+*/
+/* function toggleTable() {
   switch (
     window
       .getComputedStyle(document.getElementById("variableTable"))
@@ -44,182 +64,107 @@ function toggleTable() {
       document.getElementById("toggleTable").value = "Show Variables";
       break;
   }
+} */
+
+const variables = [
+  { variable: "%GAME", desc: "Game name" },
+  { variable: "%CAT", desc: "Category" },
+  { variable: "%RGN", desc: "Region" },
+  { variable: "%REL", desc: "Release" },
+  { variable: "%TWIT", desc: "Game, Twitch" },
+  { variable: "%SYS", desc: "System" },
+  { variable: "%EST", desc: "Estimate" },
+  { variable: "%SET", desc: "Setup Time" },
+  { variable: "%RNR", desc: "Runner" },
+  { variable: "%CCYY", desc: "Year, four digits" },
+  { variable: "%YY", desc: "Year, last two digits (00-99)" },
+  { variable: "%MM", desc: "Month as a decimal number (01-12)" },
+  { variable: "%DD", desc: "Day of the month, zero-padded (01-31)" },
+  { variable: "%hh", desc: "Hour in 24h format (00-23)" },
+  { variable: "%mm", desc: "Minute (00-59)" },
+  { variable: "%ss", desc: "Second (00-61)" },
+  { variable: "%%", desc: "A % sign" },
+  { variable: "%a", desc: "Abbreviated weekday name" },
+  { variable: "%A", desc: "Full weekday name" },
+  { variable: "%b", desc: "Abbreviated month name" },
+  { variable: "%B", desc: "Full month name" },
+  { variable: "%d", desc: "Day of the month, zero-padded (01-31)" },
+  { variable: "%H", desc: "Hour in 24h format (00-23)" },
+  { variable: "%l", desc: "Hour in 12h format (01-12)" },
+  { variable: "%m", desc: "Month as a decimal number (01-12)" },
+  { variable: "%M", desc: "Minute (00-59)" },
+  { variable: "%p", desc: "AM or PM designation" },
+  { variable: "%S", desc: "Second (00-61)" },
+  { variable: "%y", desc: "Year, last two digits (00-99)" },
+  { variable: "%Y", desc: "Year" },
+  { variable: "%z", desc: "ISO 8601 offset from UTC in timezone" },
+  { variable: "%Z", desc: "Timezone name or abbreviation" },
+  { variable: "%FPS", desc: "Frames per second" },
+  { variable: "%CRES", desc: "Base (canvas) resolution" },
+  { variable: "%ORES", desc: "Output (scaled) resolution" },
+  { variable: "%VF", desc: "Video format" },
+];
+
+const showTable = ref(false);
+
+function toggleAutoRecord() {
+  if (settings.data) {
+    settings.data.autoRecord = !settings.data.autoRecord;
+  }
+  settings.save();
+}
+
+function reset() {
+  if (autoRecord.data && settings.data) {
+    autoRecord.loadDefault();
+    settings.data.autoRecord = autoRecord.data?.active;
+    settings.save();
+  }
 }
 </script>
 <template>
-  <nodecg-button
-    id="autoRecord"
-    onClick="settings.value.autoRecord = !settings.value.autoRecord"></nodecg-button>
-  <nodecg-input
-    type="text"
-    id="filenameFormatting"
-    label="Filename Formatting"
-    onChange="autoRecord.value.filenameFormatting = this.value"></nodecg-input>
-  <nodecg-button id="toggleTable" onClick="toggleTable()"
-    >Show Variables</nodecg-button
-  >
-  <table id="variableTable">
-    <tbody>
-      <tr>
-        <td>%GAME</td>
-        <td>Game name</td>
-      </tr>
-      <tr>
-        <td>%CAT</td>
-        <td>Category</td>
-      </tr>
-      <tr>
-        <td>%RGN</td>
-        <td>Region</td>
-      </tr>
-      <tr>
-        <td>%REL</td>
-        <td>Release</td>
-      </tr>
-      <tr>
-        <td>%TWIT</td>
-        <td>Game, Twitch</td>
-      </tr>
-      <tr>
-        <td>%SYS</td>
-        <td>System</td>
-      </tr>
-      <tr>
-        <td>%EST</td>
-        <td>Estimate</td>
-      </tr>
-      <tr>
-        <td>%SET</td>
-        <td>Setup Time</td>
-      </tr>
-      <tr>
-        <td>%RNR</td>
-        <td>Runner</td>
-      </tr>
-      <tr>
-        <td>%CCYY</td>
-        <td>Year, four digits</td>
-      </tr>
-      <tr>
-        <td>%YY</td>
-        <td>Year, last two digits (00-99)</td>
-      </tr>
-      <tr>
-        <td>%MM</td>
-        <td>Month as a decimal number (01-12)</td>
-      </tr>
-      <tr>
-        <td>%DD</td>
-        <td>Day of the month, zero-padded (01-31)</td>
-      </tr>
-      <tr>
-        <td>%hh</td>
-        <td>Hour in 24h format (00-23)</td>
-      </tr>
-      <tr>
-        <td>%mm</td>
-        <td>Minute (00-59)</td>
-      </tr>
-      <tr>
-        <td>%ss</td>
-        <td>Second (00-61)</td>
-      </tr>
-      <tr>
-        <td>%%</td>
-        <td>A % sign</td>
-      </tr>
-      <tr>
-        <td>%a</td>
-        <td>Abbreviated weekday name</td>
-      </tr>
-      <tr>
-        <td>%A</td>
-        <td>Full weekday name</td>
-      </tr>
-      <tr>
-        <td>%b</td>
-        <td>Abbreviated month name</td>
-      </tr>
-      <tr>
-        <td>%B</td>
-        <td>Full month name</td>
-      </tr>
-      <tr>
-        <td>%d</td>
-        <td>Day of the month, zero-padded (01-31)</td>
-      </tr>
-      <tr>
-        <td>%H</td>
-        <td>Hour in 24h format (00-23)</td>
-      </tr>
-      <tr>
-        <td>%l</td>
-        <td>Hour in 12h format (01-12)</td>
-      </tr>
-      <tr>
-        <td>%m</td>
-        <td>Month as a decimal number (01-12)</td>
-      </tr>
-      <tr>
-        <td>%M</td>
-        <td>Minute (00-59)</td>
-      </tr>
-      <tr>
-        <td>%p</td>
-        <td>AM or PM designation</td>
-      </tr>
-      <tr>
-        <td>%S</td>
-        <td>Second (00-61)</td>
-      </tr>
-      <tr>
-        <td>%y</td>
-        <td>Year, last two digits (00-99)</td>
-      </tr>
-      <tr>
-        <td>%Y</td>
-        <td>Year</td>
-      </tr>
-      <tr>
-        <td>%z</td>
-        <td>ISO 8601 offset from UTC in timezone</td>
-      </tr>
-      <tr>
-        <td>%Z</td>
-        <td>Timezone name or abbreviation</td>
-      </tr>
-      <tr>
-        <td>%FPS</td>
-        <td>Frames per second</td>
-      </tr>
-      <tr>
-        <td>%CRES</td>
-        <td>Base (canvas) resolution</td>
-      </tr>
-      <tr>
-        <td>%ORES</td>
-        <td>Output (scaled) resolution</td>
-      </tr>
-      <tr>
-        <td>%VF</td>
-        <td>Video format</td>
-      </tr>
-    </tbody>
-  </table>
+  <div class="grid grid-cols-2 gap-2" v-if="autoRecord.data">
+    <Button
+      id="autoRecord"
+      :severity="settings.data?.autoRecord ? 'warn' : 'primary'"
+      @click="toggleAutoRecord">
+      {{ settings.data?.autoRecord ? "Disable" : "Enable" }} Auto-Record
+    </Button>
+    <FloatLabel variant="in" class="row-start-2 col-span-2">
+      <!-- <IconField> -->
+      <InputText
+        type="text"
+        id="filenameFormatting"
+        itemid="filenameFormatting"
+        class="w-full"
+        v-model="autoRecord.data.filenameFormatting"
+        @update:model-value="autoRecord.save" />
+      <!-- <InputIcon>
+          <SvgIcon type="mdi" :path="mdiCheckCircleOutline" :class="" />
+        </InputIcon>
+      </IconField> -->
+      <label for="filenameFormatting">Filename Formatting</label>
+    </FloatLabel>
+    <Button severity="danger" class="row-start-3" @click="reset">
+      Reset Format
+    </Button>
+    <Button
+      id="toggleTable"
+      :severity="showTable ? 'secondary' : 'help'"
+      class="row-start-3"
+      @click="showTable = !showTable">
+      {{ showTable ? "Hide" : "Show" }} Variables
+    </Button>
+
+    <DataTable
+      :value="variables"
+      v-show="showTable"
+      scrollable
+      scroll-height="flex"
+      size="small"
+      class="row-start-4 col-span-2 max-h-100 rounded-lg">
+      <Column field="variable" header="Variable" />
+      <Column field="desc" header="Description" />
+    </DataTable>
+  </div>
 </template>
-
-<style>
-nodecg-input {
-  margin-top: 17px;
-  margin-bottom: 12px;
-}
-
-#variableTable {
-  display: none;
-  margin-top: 15px;
-}
-
-td:first-child {
-  width: 25%;
-}
-</style>
