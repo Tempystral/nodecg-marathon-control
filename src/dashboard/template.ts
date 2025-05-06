f8b786aac991f90685817f62b4ef6f68d298ddad
Tempystral <elijah.doern@gmail.com>
2025-05-06 13:01:05 -0400

    Some final cleanup

--- src/dashboard/template.ts
+++ src/dashboard/template.ts
@@ -1,9 +1,8 @@
-import { createHead } from "@vueuse/head";
-import { Component, createApp } from "vue";
-import OBSSettingsPanel from "./panels/OBSSettingsPanel.vue";
-import PrimeVue from "primevue/config";
 import Theme from "@primeuix/themes/aura";
+import { createHead } from "@vueuse/head";
+import PrimeVue from "primevue/config";
 import Tooltip from "primevue/tooltip";
+import { Component, createApp } from "vue";
 import "./style.css";
 
 export function create(component: Component) {
